314c0d14379625f28fc25061de2214316b089ce1
Arun Durvasula <adurvasu@farm.caes.ucdavis.edu>
2013-07-08 23:27:11 -0700

    updated README. modified cleanup.sh for large inputs

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,7 @@
-rm maize_bs.sh.*
-rm out.*
-cat paramsFile.* > parameters.txt # will the parameters be out of order? ls lines the files up like: .1 .10 .2 .3 .4 ... so when I cat them will they be in that order?
-rm paramsFile.*
-cat stats.* > semifinal_stats.txt # same thing here
-rm stats.*
+find ./ -iname out.* | xargs -I '{}' mv {} /home/adurvasu/trash
+find ./paramsFile.* | xargs cat > parameters.txt
+find ./ -iname paramsFile.* | xargs -I '{}' mv {} /home/adurvasu/trash
+cat stats.* > semifinal_stats.txt
+find ./ -iname stats.* | xargs -I '{}' mv {} /home/adurvasu/trash
+python clean_stats.py
+mv semifinal_stats.txt /home/adurvasu/trash
