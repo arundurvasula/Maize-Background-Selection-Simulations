8160d63abcc18db6d1181e4a2fc0fa1a4110b5e9
Arun Durvasula <adurvasu@farm.caes.ucdavis.edu>
2013-07-16 23:45:28 -0700

    general updates

--- cleanup.sh
+++ cleanup.sh
@@ -1,12 +1,11 @@
 ARG_MAX="-n131072" #system wide max # of args. 
 
-find . -name "out.*" | xargs $ARG_MAX mv -t /home/adurvasu/trash/
-
 find . -name "paramsFile.*" | xargs $ARG_MAX cat > parameters.txt
 find . -name "paramsFile.*" | xargs $ARG_MAX mv -t /home/adurvasu/trash/
 
 find . -name "stats.*" | xargs $ARG_MAX cat > semifinal_stats.txt
 find . -name "stats.*" | xargs $ARG_MAX mv -t /home/adurvasu/trash/
 
+module load python
 python clean_stats.py
 mv semifinal_stats.txt /home/adurvasu/trash
