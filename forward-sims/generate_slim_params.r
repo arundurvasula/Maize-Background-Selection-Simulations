fbc430d21ba67ad5de99406742c6bf046a3e57c6
Arun Durvasula <ohblahitsme@yahoo.com>
2013-10-11 12:10:00 -0700

    file output

--- forward-sims/generate_slim_params.r
+++ forward-sims/generate_slim_params.r
@@ -3,6 +3,7 @@
 #TODO: Generate stepwise linear expansion
 pi_dist <- read.table("../pi_dist.txt")
 
+newline <- "\n"
 s_mut_type <- "#MUTATION TYPES\n"
 s_mut_1 <- "m1"
 n_m1_dom_coef <- 0.5
@@ -39,8 +40,12 @@ s_sample <- "R"
 n_out_size <- 500
 s_ms <- "MS"
 
-command <- paste("slim",s_mut_type, s_mut_1, n_m1_dom_coef, s_mut_fixed, n_m1_select_coef, s_mut_rate, n_ne, n_mut, 
-                 s_gen_el_type, s_gen_el_1, n_g1_mut_prop, s_chr_org, n_chr_start, n_chr_end, s_recomb_rate, n_recomb,
-                 s_gens, n_gens, s_dem_struct, n_time_start, s_new_pop, s_pop_1, n_p1_size, s_out, n_out_time, 
-                 s_sample, n_out_size, s_ms, sep=" ")
-system(command)
+command <- paste("slim",s_mut_type, s_mut_1, n_m1_dom_coef, s_mut_fixed, n_m1_select_coef, newline,
+                 s_mut_rate, n_ne, n_mut, newline,
+                 s_gen_el_type, s_gen_el_1, n_g1_mut_prop, s_chr_org, n_chr_start, n_chr_end, newline,
+                 s_recomb_rate, n_recomb, newline,
+                 s_gens, n_gens, newline,
+                 s_dem_struct, n_time_start, s_new_pop, s_pop_1, n_p1_size, newline,
+                 s_out, n_out_time, s_sample, n_out_size, s_ms, sep=" ")
+cat(command, "\n", "paramsFile.txt") #will need to pass task id variable
+system("slim paramsFile.txt")
