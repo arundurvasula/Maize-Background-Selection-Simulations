1f5e3bc08e92c42ebe9ec0422d4a5a01bb272931
Arun Durvasula <farm.cse.ucdavis.edu>
2013-10-24 15:47:14 -0700

    added slurm script

---
+++ forward-sims/slim_pipeline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -D /home/adurvasu/Maize-Background-Selection-Simulations/forward-sims/
+#SBATCH -o /home/adurvasu/Maize-Background-Selection-Simulations/forward-sims/slurm-log/slim-pipeline-out-%j.txt
+#SBATCH -e /home/adurvasu/Maize-Background-Selection-Simulations/forward-sims/slurm-log/slim-pipeline-stderr-%j.txt
+#SBATCH -J slim-pipeline
+set -e
+set -u
+module load gcc
+module load libsequence
+
+Rscript generate_slim_params.r | msstats > stats.%j
