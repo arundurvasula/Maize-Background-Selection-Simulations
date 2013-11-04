786f11e9750a80fbb0ba37dd509f03959feafcee
Arun Durvasula <farm.cse.ucdavis.edu>
2013-11-04 12:00:19 -0800

    pipeline ready

--- forward-sims/generate_slim_params.r
+++ forward-sims/generate_slim_params.r
@@ -68,6 +68,6 @@ for (i in 1:length(n_growth_gens)) {
 
 #add output line
 command <- paste(command, s_out, n_out_time, s_sample, s_pop_1, n_out_size, s_ms)
-paramsFile <- paste("paramsFile.", JOB_ID, ".txt", sep="")
+paramsFile <- paste("./slurm-log/paramsFile.", JOB_ID, ".txt", sep="")
 cat(command, "\n", file=paramsFile)  #will need to pass task id variable
 system(paste("/home/adurvasu/slim/slim ", paramsFile, sep=""))
--- forward-sims/slim_pipeline.sh
+++ forward-sims/slim_pipeline.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH -D /home/adurvasu/Maize-Background-Selection-Simulations/forward-sims/
-#SBATCH -o /home/adurvasu/Maize-Background-Selection-Simulations/forward-sims/slurm-log/slim-pipeline-out-%j.txt
-#SBATCH -e /home/adurvasu/Maize-Background-Selection-Simulations/forward-sims/slurm-log/slim-pipeline-stderr-%j.txt
+#SBATCH -o /home/adurvasu/Maize-Background-Selection-Simulations/forward-sims/slurm-log/slim-pipeline-out-%A-%j.txt
+#SBATCH -e /home/adurvasu/Maize-Background-Selection-Simulations/forward-sims/slurm-log/slim-pipeline-stderr-%A-%j.txt
 #SBATCH -J slim-pipeline
 set -e
 set -u
-module load gcc
 module load libsequence
 
-Rscript generate_slim_params.r %A | awk '/\/\// {seen = 1} seen {print}' | msstats
+Rscript generate_slim_params.r $SLURM_JOB_ID | awk '/\/\// {seen = 1} seen {print}' | msstats
