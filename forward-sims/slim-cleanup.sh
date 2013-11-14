4ae2dfc862c626f6aa4b9ed515b8f89988892262
Arun Durvasula <farm.cse.ucdavis.edu>
2013-11-13 17:44:04 -0800

    script to remove failed sims from analysis

---
+++ forward-sims/slim-cleanup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+if [ -f ./slurm-log/bneck.txt ] || [ -f ./slurm-log/stats.txt ]; then
+    echo "bneck.txt or stats.txt already exist in the slurm-log directory. Please remove them."
+    exit
+fi
+
+FILES=(`find ./slurm-log/slim-pipeline-out* -type f`)
+for file in ${FILES[@]}
+do
+    length=$(wc -l $file)
+    parsed=$(echo $length | cut -f 1 -d ' ')
+    if [ "$parsed" == "2" ]; then
+	#ugly parsing code. writes filename to stdout, extracts JOBID, removes .txt
+	#example filename: slim-pipeline-out-12397-12841.txt -> result: 12841
+	JOBID=$(echo $file | cut -f 5 -d '-' | cut -f 1 -d '.')
+	
+	cat $file >> ./slurm-log/stats.txt
+	cat './slurm-log/bneck.'$JOBID'.txt' >> ./slurm-log/bneck.txt
+    fi
+done
